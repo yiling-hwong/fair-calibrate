ca75b698bc86a06911b3a14b34f14974c3837624
yiling-hwong <yiling.hwong@gmail.com>
2026-06-16 13:03:01 +0200

    tinkering constraining 04

--- input/sampling/07_CO2-1750-sampling-ar6ch2.py
+++ input/sampling/07_CO2-1750-sampling-ar6ch2.py
@@ -24,10 +24,12 @@ load_dotenv()
 
 samples = PRIOR_SAMPLES
 
-# extrapolate the CMIP7 concs backwards
+# YLH: updated to AR7 Ch2 values (5/50/95 = 276.56/277.60/278.64 ppm, TBC)
+# YLH: AR6 was loc=278.377857, scale=2.9/NINETY_TO_ONESIGMA (90% range = 5.8 ppm)
+# YLH: AR7 loc=277.60, scale=1.04/NINETY_TO_ONESIGMA (90% range = 2.08 ppm, ~2.8x tighter)
 NINETY_TO_ONESIGMA = scipy.stats.norm.ppf(0.95)
 co2_1750_conc = scipy.stats.norm.rvs(
-    size=samples, loc=278.377857, scale=2.9 / NINETY_TO_ONESIGMA, random_state=1067061
+    size=samples, loc=277.60, scale=1.04 / NINETY_TO_ONESIGMA, random_state=1067061
 )
 
 df = pd.DataFrame({"co2_concentration": co2_1750_conc})
